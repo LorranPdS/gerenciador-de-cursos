8f7619eab22826388bbc471bfce0e517ceba69e4
LorranPdS <lorransantospereira@yahoo.com.br>
2022-12-18 11:29:45 -0300

    aula 7: sobre equals e hashcode

--- src/br/com/alura/Aluno.java
+++ src/br/com/alura/Aluno.java
@@ -13,6 +13,16 @@ public class Aluno {
 	private int numeroMatricula;
 	
 	public Aluno(String nome, int numeroMatricula) {
+		/*
+		 * Veja que, verificando se o nome é null ou não, eu estou fazendo uma programação defensiva, 
+		 * ou seja, "eu não quero que o nome corra o risco de ser null, então lanço a Exceção".
+		 * 
+		 * É interessante que, fazer essa conferência aqui no construtor é bem melhor que fazer no método "equals"
+		 */
+		if(nome == null) {
+			throw new NullPointerException("Nome não pode ser null");
+		}
+		
 		this.nome = nome;
 		this.numeroMatricula = numeroMatricula;
 	}
@@ -23,6 +33,46 @@ public class Aluno {
 
 	public int getNumeroMatricula() {
 		return numeroMatricula;
+	}
+	
+	/*
+	 * Vamos criar no equals então, o que é considerado para você quando um aluno é igual ao outro. 
+	 * Um aluno é igual quando apenas o nome é igual? Ou quando a matrícula é igual? Ou quando o nome
+	 * e a matrícula são iguais?? Então veja que, quem define quem é igual ao quê não é o Java mas sim você (o negócio)
+	 * 
+	 * O Java dirá quando uma String é igual é se cada letra é a mesma
+	 */
+	@Override
+		public boolean equals(Object obj) {
+		/*
+		 * Entenda que o equals usado pelo Set faz a busca, mas não olhando de objeto em objeto, mas sim através de um
+		 * "número mágico" que é criado para cada objeto adicionado. Sempre que o método "equals" foge do padrão (como fizemos
+		 * agora com este equals aqui), nós precisamos mudar também esse número mágico. Esse número mágico é chamado de "hashcode", 
+		 */
+		
+			Aluno outro = (Aluno) obj;
+			return this.nome.equals(outro.nome);
+		}
+	
+	/*
+	 * O hashCode é um identificador único do objeto, mas iremos calcular ele
+	 */
+	@Override
+	public int hashCode() {
+		/*
+		 * Essa maneira que está em comentário abaixo por exemplo não é uma boa forma de se criar hashCode porque ele colocará
+		 * os nomes de alunos que começam com uma determinada letra para que sejam buscados, então fazer essa busca ainda será
+		 * demorada, já que o Set vai procurar em grupos de hashCode com iniciais iguais.
+		 */
+//		return this.nome.charAt(0);
+		
+		/*
+		 * A forma criada abaixo é interessante de fazer hashCode porque, todos os caracteres que tiverem as somas iguais serão
+		 * buscados, o que diminui bastante a busca por qual nome é igual ao que se busca.
+		 * 
+		 * Saiba também que o hashCode da classe String funciona muito bem também, basta entrar no método dele para você ver como é montado.
+		 */
+		return this.nome.hashCode();
 	}
 
 	@Override
--- src/br/com/alura/TestaCursoComAluno.java
+++ src/br/com/alura/TestaCursoComAluno.java
@@ -25,5 +25,32 @@ public class TestaCursoComAluno {
 		javaColecoes.getAlunos().forEach(a -> {
 			System.out.println(a);
 		});
+		
+		// Aqui começa a aula 7. Vamos criar esse método usando hashcode e equals
+		
+		System.out.println("O aluno " + a1 + " está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(a1));
+		
+		// O grande problema de quem trabalha com Set é o Equals
+		
+		Aluno turini = new Aluno("Rodrigo Turini", 34672); // São Rodrigo diferentes
+		System.out.println("E esse Turini, está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(turini));
+		
+//		System.out.println("O a1 é == ao Turini?");
+//		System.out.println(a1 == turini); // vai dar false por serem referências de memória diferentes
+
+		System.out.println("O a1 é equals ao Turini?");
+		// Então a partir de agora eu construo o método "equals"
+		System.out.println(a1.equals(turini)); // Se você entrar nesse método equals, verá que há um igual também
+		
+		/*
+		 * A regra geral então é: se eu tenho 2 objetos que são equals, OBRIGATORIAMENTE o seguinte é TRUE, veja:
+		 */
+		System.out.println(a1.hashCode() == turini.hashCode());
+		/*
+		 * Portanto, para dois conteúdos serem considerados idênticos, ele tem que ter esse mesmo número de espalhamento (hashCode)
+		 * Importante estudar o curso "Estrutura de Dados"
+		 */
 	}
 }
